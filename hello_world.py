bebcc85403568e16bf30858fbff7139e6e5a5e5a
smellslikeml <smellslikeml>
2021-07-21 18:02:53 +0000

    new file

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC 
+# MAGIC ## Test creating new notebook
+
+# COMMAND ----------
+
+print("Hello world! This is a test")
+
+# COMMAND ----------
+
+def foo():
+  for i in range(3):
+    print(i)
+    
+foo()
+
+# COMMAND ----------
+
+
